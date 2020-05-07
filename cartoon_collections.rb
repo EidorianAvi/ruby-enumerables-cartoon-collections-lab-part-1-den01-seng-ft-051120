11cd0d3bd89c41fb8a4ff521a40fbb4b88a1d451
Rene Avila <reneavila1993@gmail.com>
2020-05-07 23:50:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,14 +1,3 @@
-# def greet_characters(array)
-#   array.each do |name|
-#     puts "Hello #{name}!"
-#   end 
-# end
-
-# def list_dwarves(array)
-#   array.each_with_index do |name, index|
-#     puts "#{index + 1}. #{name}"
-#   end
-# end
 
 def greet_characters(array)
   array.each do |name|
